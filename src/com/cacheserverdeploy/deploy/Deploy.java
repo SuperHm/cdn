3bd8dd738e1c7ff2de939c6af8c516160cfb6b69
ccding_ustc <andxdcc@sina.com>
2017-03-18 14:25:21 +0800

    modified

--- src/com/cacheserverdeploy/deploy/Deploy.java
+++ src/com/cacheserverdeploy/deploy/Deploy.java
@@ -1,18 +1,9 @@
 package com.cacheserverdeploy.deploy;
 
-import java.security.cert.TrustAnchor;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.Stack;
-
-import com.cacheserverdeploy.deploy.Graph.Edge;
 
 public class Deploy{
-	private static int lineNum = 0;
-	final static int MAX_WEIGHT = 100000;
+	private static int lineNum = 0;//网络图读取过程中标记行数
+
 	/**
      * 你需要完成的入口
      * <功能详细描述>
@@ -25,19 +16,25 @@ public class Deploy{
         graph = readProblemLine(graphContent);
     	readEdges(graphContent, graph);
     	readClients(graphContent, graph);
-    	int[] selectedServerNodes = selectServerNodes(3, graph);
+    	int[] selectedServerNodes = graph.selectServerNodes(3);
+    	int[] clientLinkedNodes = graph.getLinkedNodes();
+    	
+    	boolean isSatisfied[] = new boolean[graph.clientNodesNum];//标记消费节点带宽需求是否满足
+    	
+    	for(int selectedServerNode: selectedServerNodes){
+    		int[] unitCost = new int[clientLinkedNodes.length];
+    		int[] flow = new int[clientLinkedNodes.length];
+    		String[] shortPath = new String[clientLinkedNodes.length];
+    		graph.dijkstra(selectedServerNode, clientLinkedNodes, shortPath, unitCost, flow);
+    		System.out.println(000);
+    		
+    	}
+    	
     	
         return new String[]{"17","\r\n","0 8 0 20"};
     }
     
-    public static int[] selectServerNodes(int serverNum, Graph graph){
-    	int[] selectedServerNodes = new int[serverNum];
-    	List<Integer> serverNodes = new ArrayList<>(graph.getServerNodes());
-    	for(int i=0; i<serverNum; i++){
-    		selectedServerNodes[i] = serverNodes.remove((int)(Math.random()*(serverNodes.size()-1)));
-    	}
-    	return selectedServerNodes;
-    }
+
     
     /**
      * 读取网络节点数量、消费节点数量、服务器部署成本、网络链路数量。基于此信息初始化 Graph
@@ -67,18 +64,10 @@ public class Deploy{
     	lineNum++;//跳过空行
     	while(! (line = graphContent[lineNum++]).equals("")){
     		String[] lineStrs = line.split(" ");
-    		Graph.Edge edge = new Graph.Edge(Integer.parseInt(lineStrs[0]),//起始节点
+    		graph.addEdge(Integer.parseInt(lineStrs[0]),//起始节点
     				Integer.parseInt(lineStrs[1]),//终止节点
     				Integer.parseInt(lineStrs[2]), //链路总带宽
-    				Integer.parseInt(lineStrs[3]));//单位带宽租用费用
-    		graph.addEdge(edge.srcNode, edge.desNode, edge);
-    		/*双向边*/ 
-    		edge = new Graph.Edge(Integer.parseInt(lineStrs[1]),//起始节点
-    				Integer.parseInt(lineStrs[0]),//终止节点
-    				Integer.parseInt(lineStrs[2]), //链路总带宽
-    				Integer.parseInt(lineStrs[3]));//单位带宽租用费用
-    		graph.addEdge(edge.srcNode, edge.desNode, edge);
-    		
+    				Integer.parseInt(lineStrs[3]));//单位带宽租用费用    		
     	}
     }
     
@@ -93,63 +82,12 @@ public class Deploy{
     	while(lineNum < graphContent.length){
     		line = graphContent[lineNum++];
     		String[] lineStrs = line.split(" ");
-    		Graph.Client client = new Graph.Client(Integer.parseInt(lineStrs[0]),//消费节点
+    		graph.addClient(Integer.parseInt(lineStrs[0]),//消费节点
     				Integer.parseInt(lineStrs[1]), //相邻的网络节点
     				Integer.parseInt(lineStrs[2]));//带宽需求
-    		graph.addClient(client.node, client);
-
     	}
     }
     
-    /**
-     * 寻找图 graph 中点 src 到 点 des 最小单位租用费用的路径
-     * @param graph
-     * @param src
-     * @param des
-     */
-    public static void dijkstra(Graph graph, int src, String[] shortPath, int[] dis){
-    	int nodesNum = graph.serverNodesNum;
-    	int[][] edgesWeight = new int[nodesNum][nodesNum];
-    	Edge edge = null;
-    	for(int i=0; i<nodesNum; i++){
-    		for(int j=0; j<nodesNum; j++){
-    			edge = graph.getEdges()[i][j];
-    			if(edge != null){
-    				edgesWeight[i][j]  = edge.weight;
-    			}else{
-    				edgesWeight[i][j] = MAX_WEIGHT;
-    			}
-    		}
-    	}
-    	boolean isVisited[] = new boolean[nodesNum];//标记节点最短距离是否求出
-    	dis = new int[nodesNum];//存储节点 src 到其他节点的最短距离
-    	shortPath = new String[nodesNum];
-    	//初始化节点 src 到其他节点的距离为无穷大
-    	for(int i=0; i<dis.length; i++){
-    		dis[i] = Integer.MAX_VALUE;
-    		shortPath[i] = src+"->"+i;
-    	}
-    	
-    	isVisited[src] = true;
-    	dis[src] = 0;
-    	
-    	for(int count=1; count<nodesNum; count++){
-    		int minDis = Integer.MAX_VALUE;
-    		int nextNode = -1;
-    		for(int i=0; i<nodesNum; i++){
-				if(! isVisited[i] && edgesWeight[src][i] < minDis){
-					minDis = edgesWeight[src][i];
-					nextNode = i;
-				}
-    		}
-    		dis[nextNode] = minDis;
-    		isVisited[nextNode] = true;
-    		for(int i=0; i<nodesNum; i++){
-				if(!isVisited[i] && edgesWeight[src][nextNode]+edgesWeight[nextNode][i]< edgesWeight[src][i]){
-					edgesWeight[src][i] = edgesWeight[src][nextNode] + edgesWeight[nextNode][i];
-					shortPath[i] = shortPath[nextNode] +"->"+i;
-				}
-    		}
-    	}
-    }
+  
+   
 }
--- src/com/cacheserverdeploy/deploy/Graph.java
+++ src/com/cacheserverdeploy/deploy/Graph.java
@@ -1,8 +1,6 @@
 package com.cacheserverdeploy.deploy;
 
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
 
 /**
  * 
@@ -15,10 +13,15 @@ import java.util.Set;
  *
  */
 public class Graph {
-	private Edge[][] edges;
-	private  Client[] clients;
-	private Set<Integer> serverNodes;//服务节点
-	private Set<Integer> clientNodes;//消费节点
+	
+	final static int MAX_WEIGHT = 100000;
+
+	private int[] serverNodes;//服务节点
+	private int[] clientNodes;//消费节点
+	private int[][] bandWidths;
+	private int[][] unitCosts;
+	private int[] demands;
+	private int[] linkedNodes;
 	final int serverCost;
 	final int linkNum;
 	final int clientNodesNum;
@@ -26,64 +29,152 @@ public class Graph {
 
 	
 	public Graph(int serverNodesNum, int clientNodesNum, int serverCost, int linkNum){
-		edges = new Edge[serverNodesNum][serverNodesNum];
-		clients = new Client[clientNodesNum];
+		this.serverNodes = new int[serverNodesNum];
+		this.clientNodes = new int[clientNodesNum];
+		this.bandWidths = new int[serverNodesNum][serverNodesNum];
+		this.unitCosts = new int[serverNodesNum][serverNodesNum];
+		this.demands = new int[clientNodesNum];
+		this.linkedNodes = new int[clientNodesNum];
+		
 		this.serverCost = serverCost;
 		this.linkNum = linkNum;
 		this.clientNodesNum = clientNodesNum;
 		this.serverNodesNum = serverNodesNum;
-		this.serverNodes = new HashSet<>(this.serverNodesNum);
-		this.clientNodes = new HashSet<>(this.clientNodesNum);
+		
 		for(int i=0; i<this.serverNodesNum; i++)
-			serverNodes.add(i);
+			serverNodes[i] = i;
 		for(int i=0; i<this.clientNodesNum; i++)
-			clientNodes.add(i);
+			clientNodes[i] = i;
 	}
 	
 	
-	public void addEdge(int src, int des, Edge edge){
-		edges[src][des] = edge;
+	public void addEdge(int src, int des, int bandWidth, int unitCost){
+		this.bandWidths[src][des] = bandWidth;
+		this.bandWidths[des][src] = bandWidth;
+		this.unitCosts[src][des] = unitCost;
+		this.unitCosts[des][src] = unitCost;
+		
 	}
 	
-	public void addClient(int node, Client client){
-		clients[node] = client;
+	public void addClient(int node, int linkedNode, int demand){
+		this.linkedNodes[node] = linkedNode;
+		this.demands[node] = demand;
 	}
 	
-	public Edge[][] getEdges(){
-		return edges;
-	}
-	
-	public Client[] getClients(){
-		return clients;
-	}
-	
-	public Set<Integer> getServerNodes(){
+	public int[] getServerNodes() {
 		return serverNodes;
 	}
 
-	static class Edge{
-		int srcNode;
-		int desNode;
-		int bandWidth;
-		int unitCost;
-		int weight;
-		public Edge(int srcNode, int desNode, int bandWidth, int unitCost){
-			this.srcNode = srcNode;
-			this.desNode = desNode;
-			this.bandWidth = bandWidth;
-			this.unitCost = unitCost;
-			this.weight = unitCost;
-		}
+
+	public int[] getClientNodes() {
+		return clientNodes;
+	}
+
+
+	public int[][] getUnitCosts() {
+		return unitCosts;
+	}
+
+
+	public int[] getDemands() {
+		return demands;
+	}
+
+
+	public int[] getLinkedNodes() {
+		return linkedNodes;
+	}
+
+
+	public int[][] getBandWidths(){
+		return this.bandWidths;
 	}
 	
-	static class Client{
-		int node;
-		int neighborNode;
-		int demand;
-		public Client(int node, int neighborNode, int demand){
-			this.node = node;
-			this.neighborNode = neighborNode;
-			this.demand = demand;
-		}
-	}
+    /**
+     * 按一定策略选择初始服务器节点
+     * 
+     * @param serverNum 服务器节点个数
+     * @return 选择的服务器节点
+     */
+    public int[] selectServerNodes(int initServerNum){
+    	int[] selectedServerNodes = new int[initServerNum];
+    	boolean[] isSelected = new boolean[serverNodesNum];
+    	Random random = new Random();
+    	int selectedCount = 0;
+    	while(selectedCount != initServerNum){
+    		int tmp = random.nextInt(serverNodesNum);
+    		if(!isSelected[tmp])
+    			selectedServerNodes[selectedCount++] = tmp;
+    	}
+    	return selectedServerNodes;
+    }
+
+    /**
+     * 使用 dijkstra 算法寻找图中节点 src 到节点 des 的最小租用费用路径
+     * 
+     * @param src 开始节点
+     * @param des 终止节点集合
+     * @param shortPath 路径信息
+     * @param dis 最小单位租用费用
+     */
+    public void dijkstra(int src, int[] des, String[] shortPath, int[] dis, int[] flow){
+    	int nodesNum = this.serverNodesNum;
+    	int[][] edgesWeight = new int[nodesNum][nodesNum];
+    	int[][] maxFlow = new int[nodesNum][nodesNum];
+    	
+    	for(int i=0; i<nodesNum; i++){
+    		for(int j=0; j<nodesNum; j++){
+    			maxFlow[i][j] = bandWidths[i][j];
+    			if(unitCosts[i][j] == 0){//没有对应的边
+    				edgesWeight[i][j] = MAX_WEIGHT;
+    			}else{
+    				edgesWeight[i][j]  = unitCosts[i][j];
+    			}
+    		}
+    	}
+    	
+    	boolean isVisited[] = new boolean[nodesNum];//标记节点最短距离是否求出
+    	int[] disTmp = new int[nodesNum];
+    	String[] shortPathTmp = new String[nodesNum];
+    	int[] flowTmp = new int[nodesNum];
+    	
+    	//初始化节点 src 到其他节点的距离为无穷大
+    	for(int i=0; i<disTmp.length; i++){
+    		disTmp[i] = Integer.MAX_VALUE;
+    		shortPathTmp[i] = src+"->"+i;
+    	}
+    	
+    	isVisited[src] = true;
+    	disTmp[src] = 0;
+    	flowTmp[src] = 0;
+    	
+    	for(int count=1; count<nodesNum; count++){
+    		int minDis = Integer.MAX_VALUE;
+    		int nextNode = -1;
+    		int tmp = 0;
+    		for(int i=0; i<nodesNum; i++){
+				if(! isVisited[i] && edgesWeight[src][i] < minDis){
+					minDis = edgesWeight[src][i];
+					nextNode = i;
+					tmp = maxFlow[src][i];
+				}
+    		}
+    		disTmp[nextNode] = minDis;
+    		flowTmp[nextNode] = tmp;
+    		isVisited[nextNode] = true;
+    		//松弛操作
+    		for(int i=0; i<nodesNum; i++){
+				if(!isVisited[i] && edgesWeight[src][nextNode]+edgesWeight[nextNode][i]< edgesWeight[src][i]){
+					edgesWeight[src][i] = edgesWeight[src][nextNode] + edgesWeight[nextNode][i];
+					maxFlow[src][i] = Math.min(maxFlow[src][nextNode], maxFlow[nextNode][i]);
+					shortPathTmp[i] = shortPathTmp[nextNode] +"->"+i;
+				}
+    		}
+    	}
+    	for(int i=0; i<des.length; i++){
+    		dis[i] = disTmp[des[i]];
+    		shortPath[i] = shortPathTmp[des[i]];
+    		flow[i] = flowTmp[des[i]];
+    	}
+    }
 }
